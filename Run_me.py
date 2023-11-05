41165d731401d5b9088f3c16b356e82ec69f2c86
AIsoltani <amirsoltani_os@hotmail.com>
2023-11-05 07:52:19 +0330

    Update Run_me.py

--- Run_me.py
+++ Run_me.py
@@ -7,6 +7,7 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 from scipy.special import erf
+
 #### Part 1
 gmm ={'pi':0.5,'mu':[-1,1],'sigma':[0.5,0.5]}
 n = 1000
