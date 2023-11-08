3e5131bdf9b09c752110d289c1a4e48ecdce1e00
AIsoltani <amirsoltani_os@hotmail.com>
2023-11-08 09:08:26 +0330

    Update GMMinv.py

--- GMMinv.py
+++ GMMinv.py
@@ -9,6 +9,7 @@ def GMMinv(X, gmm, b):
     F = np.zeros((n,1))
     UU = np.zeros((n,1))
     for i in range(n):
-        F[i] = gmm['pi'] * 1/2 * (1+erf((X[i]-(gmm['mu'][0]))/((gmm['sigma'][0])*np.sqrt(2)))) + (1 - gmm['pi']) * 1/2 * (1+erf((X[i]-(gmm['mu'][1]))/((gmm['sigma'][1])*np.sqrt(2))))
+        F[i] = gmm['pi'] * 1/2 * (1+erf((X[i]-(gmm['mu'][0]))/((gmm['sigma'][0])*np.sqrt(2)))) +
+        (1 - gmm['pi']) * 1/2 * (1+erf((X[i]-(gmm['mu'][1]))/((gmm['sigma'][1])*np.sqrt(2))))
         UU[i] = norm.ppf(F[i])
     return UU
